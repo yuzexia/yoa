c38fa264fcd58d811b6dd8ae304a24a3dd464755
yuze.xia <327932801@qq.com>
2018-09-07 16:10:37 +0800

    create http server

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200)
+    res.end('hello yoa2')
+})
+
+server.listen(9092, () => {
+    console.log('server start on port 9092')
+})
