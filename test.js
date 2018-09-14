e5bd95160eb85521c226f1a3ca2ea975fa2fb067
yuze.xia <327932801@qq.com>
2018-09-14 15:15:41 +0800

    onion rings

--- test.js
+++ test.js
@@ -1,40 +1,112 @@
 /**
- * 中间件
+ * 洋葱圈模型
  */
 
-function add (x, y) {
-    return x + y;
+async function fn1(next) {
+    console.log('fn1')
+    await next()
+    console.log('end fn1')
 }
 
-function double (z) {
-    return z * 2
+async function fn2(next) {
+    console.log('fn2')
+    await delay()
+    await next()
+    console.log('end fn2')
 }
 
-// const res1 = add(1, 2)
-// const res2 = double(res1)
-// console.log(res2)
+function fn3(next) {
+    console.log('fn3')
+}
 
-// const res3 = double(add(1, 2))
-// console.log(res3)
+function delay() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() =>{
+            resolve()
+        }, 2000)
+    })
+}
 
-const middlewares = [add, double]
-let len = middlewares.length
-function compose(midds) {
-    return (...args) => {
-        // 初始值
-        console.log('...args', ...args)
-        let res = midds[0](...args)
-        console.log('res', res)
-        for (let i = 1; i < len; i++) {
-            res = midds[i](res)
+function compose (middlewares) {
+    return function () {
+        return dispatch(0)
+
+        function dispatch(i) {
+            let fn = middlewares[i]
+            if (!fn) {
+                return Promise.resolve()
+            }
+            return Promise.resolve(fn(function next() {
+                return dispatch(i + 1)
+            }))
         }
-        return res
     }
 }
 
-const fn = compose(middlewares)
-const res = fn(1,2)
-console.log(res)
+const middlewares = [fn1, fn2,fn3]
+
+const finalFn = compose(middlewares)
+
+finalFn()
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /**
+//  * 中间件
+//  */
+
+// function add (x, y) {
+//     return x + y;
+// }
+
+// function double (z) {
+//     return z * 2
+// }
+
+// // const res1 = add(1, 2)
+// // const res2 = double(res1)
+// // console.log(res2)
+
+// // const res3 = double(add(1, 2))
+// // console.log(res3)
+
+// const middlewares = [add, double]
+// let len = middlewares.length
+// function compose(midds) {
+//     return (...args) => {
+//         // 初始值
+//         console.log('...args', ...args)
+//         let res = midds[0](...args)
+//         console.log('res', res)
+//         for (let i = 1; i < len; i++) {
+//             res = midds[i](res)
+//         }
+//         return res
+//     }
+// }
+
+// const fn = compose(middlewares)
+// const res = fn(1,2)
+// console.log(res)
 
 
 
